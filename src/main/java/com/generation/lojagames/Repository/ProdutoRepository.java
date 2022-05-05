d2f86bd7c6d2c717cee63c61d3a1680e9cd39940
jessbslee <jess.b.silva@gmail.com>
2022-05-05 20:46:23 -0300

    Atualizando de novo

--- src/main/java/com/generation/lojagames/Repository/ProdutoRepository.java
+++ src/main/java/com/generation/lojagames/Repository/ProdutoRepository.java
@@ -5,29 +5,31 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.generation.lojagames.model.ProdutoModel;
 
-public interface ProdutoRepository  extends JpaRepository <ProdutoModel, Long>{
-	
+	@Repository
+	public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long> {
+
 	public List<ProdutoModel> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 
 	/**
-	 *  Método Personalizado - Buscar todos os Produtos cujo o preço seja maior 
-	 *  do que um valor digitado ordenado pelo preço em ordem crescente
-	 *  
-	 *  MySQL: select * from tb_produtos where preco > preco order by preco;
+	 * Método Personalizado - Buscar todos os Produtos cujo o preço seja maior do
+	 * que um valor digitado ordenado pelo preço em ordem crescente
+	 * 
+	 * MySQL: select * from tb_produtos where preco > preco order by preco;
 	 */
-	 
-	public List <ProdutoModel> findByPrecoGreaterThanOrderByPreco(BigDecimal preco);
-	
+
+	public List<ProdutoModel> findByPrecoGreaterThanOrderByPreco(BigDecimal preco);
+
 	/**
-	 *  Método Personalizado - Buscar todos os Produtos cujo o preço seja menor 
-	 *  do que um valor digitado ordenado pelo preço em ordem decrescente
-	 *  
-	 *  MySQL: select * from tb_produtos where preco < preco order by preco desc;
+	 * Método Personalizado - Buscar todos os Produtos cujo o preço seja menor do
+	 * que um valor digitado ordenado pelo preço em ordem decrescente
+	 * 
+	 * MySQL: select * from tb_produtos where preco < preco order by preco desc;
 	 */
-	 
-	public List <ProdutoModel> findByPrecoLessThanOrderByPrecoDesc(BigDecimal preco);
-	
+
+	public List<ProdutoModel> findByPrecoLessThanOrderByPrecoDesc(BigDecimal preco);
+
 }
