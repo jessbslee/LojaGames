91f1b8eb3aeda3c3379b4dbb614c7d52d410473f
jessbslee <jess.b.silva@gmail.com>
2022-05-11 14:02:12 -0300

    Security

--- src/main/java/com/generation/lojagames/model/ProdutoModel.java
+++ src/main/java/com/generation/lojagames/model/ProdutoModel.java
@@ -2,13 +2,16 @@ package com.generation.lojagames.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
